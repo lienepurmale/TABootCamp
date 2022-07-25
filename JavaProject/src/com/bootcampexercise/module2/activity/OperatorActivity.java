44b53c725c101adfac17c2118ff9556a36688a31
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-07-25 15:30:31 +0200

    OperatorActivity

---
+++ JavaProject/src/com/bootcampexercise/module2/activity/OperatorActivity.java
@@ -0,0 +1,10 @@
+package com.bootcampexercise.module2.activity;
+
+public class OperatorActivity {
+    public static void main (String []args){
+        int a= 10;
+        int b= 7;
+        int sum= a+b;
+        System.out.println(sum);
+    }
+}
