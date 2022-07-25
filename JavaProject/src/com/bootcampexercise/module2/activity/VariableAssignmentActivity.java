a9e8c3ff506fdf2993b1419593f67a3b5169e0d9
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-07-25 14:00:43 +0200

    Activity2

---
+++ JavaProject/src/com/bootcampexercise/module2/activity/VariableAssignmentActivity.java
@@ -0,0 +1,11 @@
+package com.bootcampexercise.module2.activity;
+
+public class VariableAssignmentActivity {
+
+    public static void main (String []args) {
+        int a= 10;
+        a = 10+2;
+        System.out.println(a);
+
+            }
+}
