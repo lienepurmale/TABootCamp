c6f37c3563499120c3a2a7ebd9588714fb77fd32
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-07-25 16:02:49 +0200

    Create FindLargest.java

---
+++ JavaProject/src/com/bootcampexercise/module2/activity/FindLargest.java
@@ -0,0 +1,12 @@
+package com.bootcampexercise.module2.activity;
+
+public class FindLargest {
+    public static void main (String []args){
+        int x = 15;
+        if (x > 15) {
+            System.out.println("X is greater than 15");
+                } else {
+            System.out.println("X is less than 15");}
+    }
+            }
+
