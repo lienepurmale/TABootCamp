8b3af29b9fe862b9698f1107196bcf60ed0c2b91
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-07-26 08:26:00 +0200

    AddWholeNum

---
+++ JavaProject/src/com/bootcampexercise/module2/activity/AddWholeNum.java
@@ -0,0 +1,11 @@
+package com.bootcampexercise.module2.activity;
+
+public class AddWholeNum {
+    public static void main (String []args){
+        int sum=50;
+        for (int i = 50; i < 100; i++) {
+            sum=sum+i;
+        }
+            System.out.println("Sum of whole numbers from 50 to is 100 is " + sum);
+    }
+}
