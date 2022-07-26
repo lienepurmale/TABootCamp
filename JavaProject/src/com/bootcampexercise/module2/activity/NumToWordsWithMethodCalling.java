2edce40dceff2eef7999014543b1e100fb086df4
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-07-26 10:58:31 +0200

    Create NumToWordsWithMethodCalling.java

---
+++ JavaProject/src/com/bootcampexercise/module2/activity/NumToWordsWithMethodCalling.java
@@ -0,0 +1,43 @@
+package com.bootcampexercise.module2.activity;
+
+public class NumToWordsWithMethodCalling {
+    public static void main(String args[]) {
+       NumToWordsLogic(7);}
+
+    public static void NumToWordsLogic(int x) {
+        switch(x) {
+                case 1:
+                    System.out.println("One");
+                    break;
+                case 2:
+                    System.out.println("Two");
+                    break;
+                case 3:
+                    System.out.println("Three");
+                    break;
+                case 4:
+                    System.out.println("Four");
+                    break;
+                case 5:
+                    System.out.println("Five");
+                    break;
+                case 6:
+                    System.out.println("Six");
+                    break;
+                case 7:
+                    System.out.println("Seven");
+                    break;
+                case 8:
+                    System.out.println("Eight");
+                    break;
+                case 9:
+                    System.out.println("Nine");
+                    break;
+                case 10:
+                    System.out.println("Ten");
+                    break;
+                default: System.out.println("Out of scope");
+            }
+            ;
+        }
+}
