acc7f77b1c4743ffb25b03d5779c7cdaa71859e0
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-07-26 15:15:24 +0200

    Update MultiplicationTable.java

--- JavaProject/src/com/bootcampexercise/module2/activity/MultiplicationTable.java
+++ JavaProject/src/com/bootcampexercise/module2/activity/MultiplicationTable.java
@@ -5,7 +5,7 @@ public class MultiplicationTable {
 
 
                    // Table from 1 to 10
-                   for (int i = 10; i <= 20; i++) {
+                   for (int i = 11; i <= 20; i++) {
                        System.out.println("*** Table of "+i + " ***");
                        for (int j = 1; j <= 10; j++) {
                            System.out.println(i + "X" + j +" = " + i*j);
