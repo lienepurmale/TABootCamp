cded0a7531149b07199c5b3a755b65a64bbb1cc1
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-07-26 08:58:03 +0200

    MultiplicationTable

--- JavaProject/src/com/bootcampexercise/module2/activity/AddWholeNum.java
+++ JavaProject/src/com/bootcampexercise/module2/activity/AddWholeNum.java
@@ -3,7 +3,7 @@ package com.bootcampexercise.module2.activity;
 public class AddWholeNum {
     public static void main (String []args){
         int sum=50;
-        for (int i = 50; i < 100; i++) {
+        for (int i = 50; i < 101; i++) {
             sum=sum+i;
         }
             System.out.println("Sum of whole numbers from 50 to is 100 is " + sum);
---
+++ JavaProject/src/com/bootcampexercise/module2/activity/MultiplicationTable.java
@@ -0,0 +1,17 @@
+package com.bootcampexercise.module2.activity;
+
+public class MultiplicationTable {
+       public static void main(String[] args) {
+
+
+                   // Table from 1 to 10
+                   for (int i = 10; i <= 20; i++) {
+                       System.out.println("*** Table of "+i + " ***");
+                       for (int j = 1; j <= 10; j++) {
+                           System.out.println(i + "X" + j +" = " + i*j);
+                       }
+                                }
+           }
+
+       }
+
