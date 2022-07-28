2a2aa32444447db0be64fc3a8281ebd77b4df08e
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-07-28 20:52:54 +0200

    CustomExceptionActivity

---
+++ JavaProject/src/com/bootcampexercise/module7/activity/CustomExceptionActivityTest.java
@@ -0,0 +1,33 @@
+package com.bootcampexercise.module7.activity;
+
+public class CustomExceptionActivityTest {
+    public static void main(String[] args) throws CustomExceptionActivity {
+        CustomExceptionActivityTest user = new CustomExceptionActivityTest();
+        user.validateUser("John");
+        user.validateUser("Mike");
+        user.validateUser("Shanti");
+        user.validateUser("Liene");
+    }
+
+    void validateUser(String name) throws CustomExceptionActivity {
+        String[] validUsers = {"John", "Mike", "Shanti", "Stacie"};
+//flag stores 1 if a match is found else it should remain 0
+        int flag = 0;
+        for (int i = 0; i < 4; i++) {
+            if (validUsers[i] == name) {
+                flag = 1;
+            }
+            //check if parameter name contains a value which is found in validUsers array and change flag's value accordingly
+            int flag = 0;
+            for (int i = 0; i < 4; i++) {
+                if (name == validUsers[i]) {
+                    flag = 1;
+                }
+// to check if flag is one, print a message "Welcome to Payroll program"
+                else if(flag==1){
+                    System.out.println("Welcome to Payroll program");
+
+            }
+        }
+    }
+}
