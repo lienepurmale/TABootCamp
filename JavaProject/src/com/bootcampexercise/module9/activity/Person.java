1276bf81dff03db50f5eeac534a6d72e55b6ece0
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-07-28 19:50:37 +0200

    Person

--- JavaProject/src/com/bootcampexercise/module9/activity/Person.java
+++ JavaProject/src/com/bootcampexercise/module9/activity/Person.java
@@ -1,16 +1,12 @@
 package com.bootcampexercise.module9.activity;
 
 public class Person {
-
     private int weight;
     private float height;
-
-    //TODO: Create getters and setters for weight and height
 
     public int getWeight() {
         return weight;
     }
-
     public void setWeight(int weight) {
         this.weight = weight;
     }
@@ -18,19 +14,13 @@ public class Person {
     public float getHeight() {
         return height;
     }
-
     public void setHeight(float height) {
         this.height = height;
     }
 
-    //TODO: Create a method to calculate humans body mass index with two parameters for height and weight
-    //TODO: return String which indicates persons BMI. Calculation should be used weight/(height*height)
-    // TODO: Please add some checks for max weight and height (kg and meter are their units)
-    //TODO: Also add some checks that value shouldn't be negative and 0
-
     public double getBMIndex(int weight, float height) {
         double BMI = 0;
-        if (weight > 0 && height > 0 && weight < 100 && height < 210) {
+        if (height > 0 && weight > 0 && weight < 100 && height < 2) {
             BMI = weight / Math.sqrt(height);
             System.out.println("Result " + BMI + " BMI");
         } else if (weight > 100 && height > 2) {
--- JavaProject/src/com/bootcampexercise/module9/activity/PersonTest.java
+++ JavaProject/src/com/bootcampexercise/module9/activity/PersonTest.java
@@ -1,13 +1,12 @@
 package com.bootcampexercise.module9.activity;
 
 import junit.framework.TestCase;
-
 public class PersonTest extends TestCase {
 
     private Person person;
 
     //TODO: create set up
-    protected void SetUp() throws Exception {
+    protected void setUp() throws Exception {
         super.setUp();
         person = new Person();
     }
@@ -19,58 +18,50 @@ public class PersonTest extends TestCase {
     }
 
     //TODO: create unit tests for getters and setters
-    public void GetSetHeightTest() {
-        int height1 = 120;
-        person.setWeight(height1);
-        assertEquals(0, person.getHeight()); //Positive testing for height
+    public void testGetSetHeight() {
+        float height1 = 1.2f;
+        person.setHeight(height1);
+        assertEquals(height1, person.getHeight()); //Positive testing for height
     }
-
-    public void GetSetHeightTest2() {
-        int height2 = 220;
-        person.setWeight(height2);
-        assertEquals(0, person.getHeight()); //Negative testing for height
-    }
-
-    public void GetSetWeightTest() {
-        int weight1 = 45;
+    public void testGetSetWeight() {
+        int weight1 = 120;
         person.setWeight(weight1);
-        assertEquals(0, person.getWeight()); //Positive testing for Weight
+        assertEquals(weight1, person.getWeight());
     }
 
-    public void GetSetWeightTest2() {
+    public void testGetSetWeight2() {
         int weight2 = 120;
         person.setWeight(weight2);
         assertEquals(0, person.getWeight()); //Negative testing for Weight
     }
 
     //TODO: create unit test for getBodyMassIndex method
-
-    public void bmiTest1() {
+    public void test1BMI() {
         double BMI = person.getBMIndex(0, 0);
         assertEquals(0.0, BMI);
     }
 
-    public void bmiTest2() {
+    public void test2BMI() {
         double BMI = person.getBMIndex(60, 1f);
         double bmiExpected = 100 / Math.sqrt(-2.0);
         assertEquals(bmiExpected, BMI);
     }
 
-    public void bmiTest3() {
+    public void test3BMI() {
         double BMI = person.getBMIndex(120, 2f);
         double bmiExpected = 100 / Math.sqrt(-2.0);
         assertEquals(bmiExpected, BMI);
     }
 
-    public void bmiTest4() {
+    public void test4BMI() {
         double BMI = person.getBMIndex(60, 2.5f);
-        double bmiExpected = 100 / Math.sqrt(-2.0);
+        double bmiExpected = 50 / Math.sqrt(-2.0);
         assertEquals(bmiExpected, BMI);
     }
 
-    public void bmiTest5() {
+    public void test5BMI() {
         double BMI = person.getBMIndex(-60, 1f);
-        double bmiExpected = -100 / Math.sqrt(-2.0);
+        double bmiExpected = -50/ Math.sqrt(-2.0);
         assertEquals(bmiExpected, BMI);
     }
 }
