44bb986568e476ed869d34036f29e34198191cf4
lienepurmale <96015681+lienepurmale@users.noreply.github.com>
2022-07-29 10:52:13 +0200

    Update Person.java

--- JavaProject/src/com/bootcampexercise/module10/finalactivity/Person.java
+++ JavaProject/src/com/bootcampexercise/module10/finalactivity/Person.java
@@ -1,9 +1,11 @@
 package com.bootcampexercise.module10.finalactivity;
-
+//Create Person class (It should be abstract class)
+//Create attributes in Person class: name, age
 public abstract class Person {
     private int age;
     private String name;
 
+    // Create setters and getters for attributes
     public int getAge() {
         return age;
 
@@ -17,5 +19,6 @@ public abstract class Person {
     public void setName(String name) {
         this.name = name;
     }
+    //Define introduce method
     public abstract void personIntro();
 }
